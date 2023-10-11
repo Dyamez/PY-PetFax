5034243263171f49f80aded879cb7591de3fadc4
Dyamez <dyamez@outlook.com>
2023-10-10 19:12:26 -0700

    fixed

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask # , render_template
+
+# app=Flask(__name__,template_folder='./templates')
 
 def create_app():
     app = Flask(__name__)
--- petfax/pet.py
+++ petfax/pet.py
@@ -1,12 +1,13 @@
-from flask import ( Blueprint, render_template )
+from flask import Blueprint, render_template 
+
 import json
 
 pets = json.load(open('pets.json'))
 print(pets)
 
-bp = Blueprint('pet', __name__, url_prefix="/pets")
+bp = Blueprint('pet', __name__, url_prefix="/pets", template_folder='../templates') # template_folder='templates', 
 
 @bp.route('/')
 def index(): 
-    return render_template('index.html', pets=pets)
+    return render_template('index.html', pets=pets)  #render_template pets=pets
 
