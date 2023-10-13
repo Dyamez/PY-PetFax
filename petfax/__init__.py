4c5d94eaef1c28cdf369b31649bacdcfee32590e
Dyamez <dyamez@outlook.com>
2023-10-12 19:28:10 -0700

    fix

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,9 +1,19 @@
 from flask import Flask # , render_template
 
 # app=Flask(__name__,template_folder='./templates')
+from flask_migrate import Migrate
+
+
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:chickboyz@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from .import models
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
 
     @app.route('/')
     def hello():
